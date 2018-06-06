3e81b9a48049e23d58337e7e3f1f792a2f670322
Alexey Mints <mints@mps.mpg.de>
2018-06-06 15:44:45 +0200

    Added Gaia photometry support

--- unidam/utils/constants.py
+++ unidam/utils/constants.py
@@ -10,10 +10,12 @@ Some useful constants.
 from numpy import arange
 # Extinctions references:
 # JHK (2MASS), W1-W2 (AllWISE), ugriz (SDSS): Yuan et al. 2013
+# GaiaDR2 from PARSEC
 # UBVRI: Cardelli, Clayton and Mathis 1989
 R_FACTORS = {'J': 0.72, 'H': 0.46, 'K': 0.306,
              'W1': 0.18, 'W2': 0.16,
              'u': 4.39, 'g': 3.30, 'r': 2.31, 'i': 1.71, 'z': 1.29,
+             'G': 0.85926 * 3.1, 'G_BP': 1.06794 * 3.1, 'G_RP': 0.65199 * 3.1,
              'U': 4.7471, 'B': 4.1075, 'V': 3.1, 'R': 2.32, 'I': 1.49}
 
 AGE_RANGE = arange(6.13, 11.13, 0.02)

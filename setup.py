b0dc2c6ef7fed8cdd613f3b2ba77f9caa4dc40ac
Alexey Mints <mints@mps.mpg.de>
2018-09-08 17:29:21 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+import subprocess
 from setuptools import find_packages
-from numpy.distutils.core import setup, Extension
+from numpy.distutils.core import setup, Extension, build_py
 
 model_fitter = Extension('unidam.iso.model_fitter',
-                         sources=['unidam/iso/model_fitter.f90'])
+                         sources=['unidam/iso/model_fitter.f90'],
+                         depends=['unidam/iso/Solve_NonLin.f90'])
+
+
+class BuildPyCommand(build_py.build_py):
+  """Custom build command."""
+  def run(self):
+    result = subprocess.run(['./build_fortran.sh'], #shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            universal_newlines=True)
+    for s in [result.stdout, result.stderr]:
+        if len(s) > 0:
+            print(s)
+    build_py.build_py.run(self)
+
 
 setup(name='unidam',
       version='1.0',
@@ -13,14 +29,20 @@ setup(name='unidam',
       author_email='mints@mps.mpg.de',
       packages=find_packages(),
       package_data={'unidam.iso': ['unidam.conf',
-                                   'unidam_pdf.conf'],
+                                   'unidam_pdf.conf',
+                                   '*.so'],
                     '': ['DEPENDENCIES']},
       include_package_data=True,
-      ext_modules=[model_fitter],
+      #ext_modules=[model_fitter],
       install_requires=[
           'numpy', 'scipy', 'astropy',
           'simplejson', 'healpy', 'matplotlib',
-          'configparser', 'colorlog', 'argparse'
+          'colorlog', 'argparse'
       ],
-      scripts=['unidam/iso/unidam_runner.py'],
+      scripts=['unidam/iso/unidam_runner.py',
+               'unidam/tools/plot_2d_correlations.py',
+               'unidam/tools/plot_parts.py',
+               'unidam/tools/plot_sed.py',
+               'unidam/tools/prepare_data.py'],
+      cmdclass={'build_py': BuildPyCommand},
       zip_safe=False)

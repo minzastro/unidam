2d12b5b412273a57e8dca111349ada1a6d8dd499
Alexey Mints <mints@mps.mpg.de>
2019-01-11 15:28:45 +0100

    Config update

--- unidam/iso/unidam1.sh
+++ unidam/iso/unidam1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./unidam_runner.py -c unidam_pdf.conf -i ~/data/prepared/$1.fits -d --id="$2"
-python plot_parts.py -i "$2"
+./unidam_runner.py -c unidam_mini.conf -i ~/data/prepared/$1.fits -d --id="$2"
+python3 ../tools/plot_parts.py -i "$2"
 feh dump/dump_$2age.png

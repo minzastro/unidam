37f23746618c3303c91befe7a27303da6db0cc02
Alexey Mints <amints@aip.de>
2020-11-16 21:06:17 +0100

    minor cleanup

--- unidam/utils/trunc_revexpon.py
+++ unidam/utils/trunc_revexpon.py
@@ -1,11 +1,4 @@
 """An implementation of the truncated exponent with modulus distribution for SciPy"""
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import absolute_import
-# All rights reserved.
-from future import standard_library
-standard_library.install_aliases()
 import numpy as np
 from scipy.stats import rv_continuous
 

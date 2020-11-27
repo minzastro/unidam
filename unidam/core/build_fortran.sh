4724d4aa710a82b23700b748ab7965293e559905
Alexey Mints <amints@aip.de>
2020-11-27 14:24:45 +0100

    Minor update

--- unidam/core/build_fortran.sh
+++ unidam/core/build_fortran.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 gfortran -fPIC -c -o Solve_NonLin.o Solve_NonLin.f90 
 f2py --opt="-O3"  -c -m model_fitter Solve_NonLin.o model_fitter.f90
+cd ../utils
+f2py --opt="-O3"  -c -m extra_functions extra_functions.f90
+cd ..
